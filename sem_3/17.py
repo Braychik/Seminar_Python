4dd101d63ff6f5ea63a9a1df36a71eeb4301cc8a
Николай <ibraevnikolay@mail.ru>
2023-04-19 18:08:50 +0500

    задачи на семинаре и домашнее задание

---
+++ sem_3/17.py
@@ -0,0 +1,8 @@
+num = [input() for i in range(int(input()))]
+
+# num = []
+# for i in range(int(input())):
+#     num.append(int(input()))
+
+
+print(len(set(num)))
---
+++ sem_3/21.py
@@ -0,0 +1,26 @@
+# ------------------------------------------------
+
+# Напишите программу для печати всех уникальных значений в словаре.
+#
+# Input:  [{"V": "S001"}, {"V": "S002"},
+#          {"VI": "S001"}, {"VI": "S005"},
+#          {"VII": " S005 "}, {" V ":" S009 "},
+#          {" VIII ":" S007 "}]
+#
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+list_dict = [{"V": "S001"}, {"V": "S002"},
+             {"VI": "S001"}, {"VI": "S005"},
+             {"VII": " S005 "}, {"V": " S009 "},
+             {"VIII": " S007 "}]
+
+print(set([list(i.values())[0].strip() for i in list_dict]))
+
+n_list = []
+for i in list_dict:
+	w_list = list(i.values())
+	word = w_list[0]
+	word_clear = word.strip()
+	n_list.append(word_clear)
+	
+print(set(n_list))
